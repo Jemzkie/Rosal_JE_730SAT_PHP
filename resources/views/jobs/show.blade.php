518634e3680fb1c0da2540ce7cd231e959e2e632
Jemzkie <jemrowynn506@gmail.com>
2024-10-01 17:09:33 +0800

    1001_Vid18_Edit-update-and-delete-a-resource

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,15 +1,15 @@
 <x-layout>
     <x-slot:heading>
-        Job Listings
+        Job
     </x-slot:heading>
 
-    <ul>
-        @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
-                </a>
-            </li>
-        @endforeach
-    </ul>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
+
+    <p>
+        This job pays {{ $job->salary }} per year.
+    </p>
+
+    <p class="mt-6">
+       <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+    </p>
 </x-layout>
