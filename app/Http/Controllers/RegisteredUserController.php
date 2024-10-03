5cf52b13a26bcc7afc75e4f5b6a3d4680c4c4eb7
Jemzkie <jemrowynn506@gmail.com>
2024-10-03 20:46:39 +0800

    1003_Vid22_make-a-login-and-register-from-scratch-part-2

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -1,14 +1,24 @@
 <?php
 namespace App\Http\Controllers;
+
+use App\Models\User;
 use Illuminate\Http\Request;
-class SessionController extends Controller
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
+class RegisteredUserController extends Controller
 {
-    public function create()
-    {
-        return view('auth.login');
-    }
+
     public function store()
     {
-        dd(request()->all());
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name'  => ['required'],
+            'email'      => ['required', 'email'],
+            'password'   => ['required', Password::min(6), 'confirmed']
+        ]);
+        $user = User::create($attributes);
+        Auth::login($user);
+        return redirect('/jobs');
     }
 }
