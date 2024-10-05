79f2d71fab71b3ee14eafc643d62848367937139
Jemzkie <jemrowynn506@gmail.com>
2024-10-05 22:15:58 +0800

    1005_Vid26_Get-your-frontend-build-process-in-order

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -10,6 +10,7 @@
 
                 <div>
                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
+                    <strong class="text-laracasts">{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
                 </div>
             </a>
         @endforeach
